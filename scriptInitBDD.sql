4309b00818e007edfd875fb9e3749117f721e6f3
damien leloup <d.leloup.95@gmail.com>
2017-09-27 16:12:57 +0200

    putain

--- scriptInitBDD.sql
+++ scriptInitBDD.sql
@@ -1,3 +1,5 @@
+--create or replace type array_int as array(256) of integer;
+
 DECLARE
    i ordsys.ordimage;
   ctx RAW(400) := NULL;
@@ -9,25 +11,42 @@ DECLARE
   sig2 ordsys.ordimageSignature;
   sim integer;
   dist float;
+  
+  --type array_int is array(256) of integer;
+  histoR array_int;
+  histoG array_int;
+  histoB array_int;
+  
+  tauxR float;
+  tauxG float;
+  tauxB float;
+  
+  mIsColor integer;
+  mNb_pixels_contour integer;
+  mMoy_norme_grad float;
+  
 BEGIN
    FOR compteur IN 1 .. 500 LOOP
- 
-    -- insertion d'une image, de contenu vide
+    
     insert into multimedia(nom, image, signature)
     values (compteur||'.jpg', ordsys.ordimage.init(), ordsys.ordimageSignature.init());
     commit;
-    -- chargement du contenu de l'image a partir du fichier
-    select image into i
-    from multimedia
-    where nom = compteur||'.jpg'
-    for update;
-    i.importFrom(ctx, 'file', 'IMG', compteur||'.jpg');
-    update multimedia
-    set image = i
-    where nom = compteur||'.jpg';
-    commit;
- 
-  END LOOP;
+   END LOOP;
+   
+  FOR compteur IN 1 .. 500 LOOP
+       -- chargement du contenu de l'image a partir du fichier
+      select image into i
+      from multimedia
+      where nom = compteur||'.jpg'
+      for update;
+      i.importFrom(ctx, 'file', 'IMG', compteur||'.jpg');
+      update multimedia
+      set image = i
+      where nom = compteur||'.jpg';
+      commit;
+   END LOOP;
+   
+   
  
   -- generation des signatures
   for ligne in mm loop
